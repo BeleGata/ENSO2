174cb7346aaa953fe401f4bede6154cbcdf43516
vitorialiraa <mvnl1@aluno.ifal.edu.br>
2022-09-12 22:51:59 +0000

    teste

--- api/src/main/java/app/App.java
+++ api/src/main/java/app/App.java
@@ -10,7 +10,7 @@ public class App extends Jooby {
     mvc(new Divisao() );
     mvc(new Subtracao());
     mvc(new Adicao());
-    mvc(new Multiplicacao3());
+    mvc(new Multiplicacao());
     
   }
  
--- api/src/main/java/app/Multiplicacao.java
+++ api/src/main/java/app/Multiplicacao.java
@@ -5,7 +5,7 @@ import io.jooby.annotations.*;
 import io.jooby.exception.BadRequestException;
  
 @Path("/multiplicacao/{op}/{op2}")
-public class Multiplicacao3 {
+public class Multiplicacao {
   
    @GET
    public double calcula(@PathParam("op") String opStr, @PathParam("op2") String opStr2) {
