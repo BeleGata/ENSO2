0ffdf70bca0404481bf7ab4a1c68f25e08088aef
vitorialiraa <mvnl1@aluno.ifal.edu.br>
2022-09-12 23:01:19 +0000

    Multiplicar

--- api/src/main/java/app/Multiplicar.java
+++ api/src/main/java/app/Multiplicar.java
@@ -5,7 +5,7 @@ import io.jooby.annotations.*;
 import io.jooby.exception.BadRequestException;
  
 @Path("/multiplicacao/{op}/{op2}")
-public class Multiplicacao {
+public class Multiplicar {
   
    @GET
    public double calcula(@PathParam("op") String opStr, @PathParam("op2") String opStr2) {
