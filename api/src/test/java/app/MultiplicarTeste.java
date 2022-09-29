add6aa4d12523f45c4cc4eda630518eac393bd45
vitorialiraa <mvnl1@aluno.ifal.edu.br>
2022-09-29 21:39:43 +0000

    Teste da multiplicação

---
+++ api/src/test/java/app/MultiplicarTeste.java
@@ -0,0 +1,20 @@
+package app;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MultiplicarTeste {
+    
+    @Test
+    public void verificacaoPrimeiroteste(){
+        Multiplicar multiplicar = new Multiplicar();
+        double resultado = multiplicar.calcula ("5.7", "2.4");
+        assertEquals(13.68, resultado);
+    }
+    @Test
+    public void verificacaoSegundoteste(){
+        Multiplicar multiplicar = new Multiplicar();
+        double resultado = multiplicar.calcula ("19", "-5");
+        assertEquals(-95, resultado);
+    }
+}
