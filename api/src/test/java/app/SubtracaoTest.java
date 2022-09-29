6512e62399987c9c32584890f32ee95d792bd1d9
juliagabriele <102989759+juliagabriele@users.noreply.github.com>
2022-09-29 19:08:20 -0300

    Update SubtracaoTest.java

--- api/src/test/java/app/SubtracaoTest.java
+++ api/src/test/java/app/SubtracaoTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SubtracaoTest {
+    
     @Test
     public void teste1(){
         Subtracao subtracao = new Subtracao();
